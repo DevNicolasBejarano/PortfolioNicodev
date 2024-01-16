bef3f57e13130f9c9e4b83580fb33c8e1d1919e2
nicol <DevNicolasBejarano@gmail.com>
2024-01-16 11:57:37 -0500

    remove works

--- src/components/Work/Work.data.tsx
+++ src/components/Work/Work.data.tsx
@@ -19,76 +19,74 @@ export const cardContent = [
       },
     ]
   },
-  {
-    id: 1,
-    title: "Portfolio React",
-    description:
-      "This is a description2, sjdnfsdf idfs sdhfbsdf jds fjsd gf sjdgns",
-    imageUrl: "/work-2.jpeg",
-    skills: [
-      {
-        icon: <SiNextdotjs size="40" />
-      },
-      {
-        icon: <SiTailwindcss size="40" />
-      },
-      {
-        icon: <SiGithub size="40" />
-      },
+  // {
+  //   id: 1,
+  //   title: "Portfolio React",
+  //   description:
+  //     "This is a description2, sjdnfsdf idfs sdhfbsdf jds fjsd gf sjdgns",
+  //   imageUrl: "/work-2.jpeg",
+  //   skills: [
+  //     {
+  //       icon: <SiNextdotjs size="40" />
+  //     },
+  //     {
+  //       icon: <SiTailwindcss size="40" />
+  //     },
+  //     {
+  //       icon: <SiGithub size="40" />
+  //     },
+  //   ]
+  // },
+  // {
+  //   id: 2,
+  //   title: "Strapi",
+  //   description:
+  //     "This is a description, sjdnfsdf idfs sdhfbsdf jds fjsd gf sjdgns",
+  //   imageUrl: "/work-3.jpeg",
+  //   skills: [
+  //     {
+  //       icon: <SiNextdotjs size="40" />
+  //     },
+  //     {
+  //       icon: <SiTailwindcss size="40" />
+  //     },
+  //     {
+  //       icon: <SiGithub size="40" />
+  //     },
+  //   ]
+  // },
+  // {
+  //   id: 3,
+  //   title: "Prestashop",
+  //   description:
+  //     "This is a description, sjdnfsdf idfs sdhfbsdf jds fjsd gf sjdgns",
+  //   imageUrl: "/work-4.jpeg",
+  //   skills: [
+  //     {
+  //       icon: <SiNextdotjs size="40" />
+  //     },
+  //     {
+  //       icon: <SiTailwindcss size="40" />
+  //     },
+  //     {
+  //       icon: <SiGithub size="40" />
+  //     },
+  //   ]
+  // },
+  // {
+  //   id: 4,
+  //   title: "E-Commerce React",
+  //   description:
+  //     "This is a description, sjdnfsdf idfs sdhfbsdf jds fjsd gf sjdgns",
+  //   imageUrl: "/work-5.jpeg",
+  //   skills: [
+  //     {
+  //       icon: <SiNextdotjs size="40" />
+  //     },
+  //     {
+  //       icon: <SiTailwindcss size="40" />
+  //     },
+  //     {
+  //       icon: <SiGithub size="40" />
+  //     },
     ]
-  },
-  {
-    id: 2,
-    title: "Strapi",
-    description:
-      "This is a description, sjdnfsdf idfs sdhfbsdf jds fjsd gf sjdgns",
-    imageUrl: "/work-3.jpeg",
-    skills: [
-      {
-        icon: <SiNextdotjs size="40" />
-      },
-      {
-        icon: <SiTailwindcss size="40" />
-      },
-      {
-        icon: <SiGithub size="40" />
-      },
-    ]
-  },
-  {
-    id: 3,
-    title: "Prestashop",
-    description:
-      "This is a description, sjdnfsdf idfs sdhfbsdf jds fjsd gf sjdgns",
-    imageUrl: "/work-4.jpeg",
-    skills: [
-      {
-        icon: <SiNextdotjs size="40" />
-      },
-      {
-        icon: <SiTailwindcss size="40" />
-      },
-      {
-        icon: <SiGithub size="40" />
-      },
-    ]
-  },
-  {
-    id: 4,
-    title: "E-Commerce React",
-    description:
-      "This is a description, sjdnfsdf idfs sdhfbsdf jds fjsd gf sjdgns",
-    imageUrl: "/work-5.jpeg",
-    skills: [
-      {
-        icon: <SiNextdotjs size="40" />
-      },
-      {
-        icon: <SiTailwindcss size="40" />
-      },
-      {
-        icon: <SiGithub size="40" />
-      },
-    ]
-  },
-];
